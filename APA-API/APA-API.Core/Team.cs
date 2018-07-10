f11e01b7b810dfa3f2f93f4804af48941d143409
Tyler Small <tylersmall.user@gmail.com>
2018-07-10 13:30:10 -0500

    Create Team object

---
+++ APA-API/APA-API.Core/Team.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace APA_API.Core
+{
+    public class Team
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int DivisionId { get; set; }
+        public int Points { get; set; }
+
+        [Timestamp]
+        public byte[] Version { get; set; }
+    }
+}
