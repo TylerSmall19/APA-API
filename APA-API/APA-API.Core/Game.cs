04f02ba06f63156edc5ba25cb234a64bfd2cc01e
Tyler Small <tylersmall.user@gmail.com>
2018-07-10 14:15:36 -0500

    Add Game and Special loss objects w/ annotations

---
+++ APA-API/APA-API.Core/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace APA_API.Core
+{
+    public class Game
+    {
+        public int ID { get; set; }
+        [Required]
+        public int PlayerMatchId { get; set; }
+        public PlayerMatch PlayerMatch { get; set; }
+        [Required]
+        public int Innings { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int SpecialLossId { get; set; }
+        public SpecialLoss SpecialLoss { get; set; }
+        public int PlayerACoaches { get; set; }
+        public int PlayerBCoaches { get; set; }
+
+        [Timestamp]
+        public byte[] Version { get; set; }
+    }
+}
---
+++ APA-API/APA-API.Core/SpecialLoss.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APA_API.Core
+{
+    public class SpecialLoss
+    {
+        public int ID { get; set; }
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [MaxLength(75)]
+        public string Description { get; set; }
+    }
+}
