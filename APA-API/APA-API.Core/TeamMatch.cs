6606b148f9bbf3117e9072fa9c2088ea55624dff
Tyler Small <tylersmall.user@gmail.com>
2018-07-10 12:25:23 -0500

    Create TeamMatch model -- will not build without Team object

--- APA-API/APA-API.Core/TeamMatch.cs
+++ APA-API/APA-API.Core/TeamMatch.cs
@@ -1,8 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System;
 
 namespace APA_API.Core
 {
     public class TeamMatch
     {
+        public int ID { get; set; }
+        public string Location { get; set; }
+        public Team HostTeam { get; set; }
+        public Team TeamA { get; set; }
+        public Team TeamB { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int WeekNumber { get; set; }
+        public bool IsPlayoffMatch { get; set; }
+
+        [Timestamp]
+        public byte[] Version { get; set; }
     }
 }
