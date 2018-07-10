6b8aace1feecacc134d210c71ef6c4baae6fabcb
Tyler Small <tylersmall.user@gmail.com>
2018-07-10 13:48:17 -0500

    Add PlayerMatch object w/ annotations

---
+++ APA-API/APA-API.Core/PlayerMatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace APA_API.Core
+{
+    public class PlayerMatch
+    {
+        public int ID { get; set; }
+        public TeamMatch TeamMatch { get; set; }
+        [Required]
+        public Player PlayerA { get; set; }
+        [Required]
+        public Player PlayerB { get; set; }
+        [Required]
+        public int PlayerAMustWin { get; set; }
+        [Required]
+        public int PlayerBMustWin { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        [Timestamp]
+        public byte[] Version { get; set; }
+    }
+}
