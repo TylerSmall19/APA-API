0324e8cd341cefa5a264ab4e2311f73ef9364b7c
Tyler Small <tylersmall.user@gmail.com>
2018-07-10 13:32:38 -0500

    Add data annotations to Team

--- APA-API/APA-API.Core/Team.cs
+++ APA-API/APA-API.Core/Team.cs
@@ -6,6 +6,7 @@ namespace APA_API.Core
     public class Team
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DivisionId { get; set; }
         public int Points { get; set; }
