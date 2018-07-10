c8133e291314da29d407d83d8a61a33a174b3f36
Tyler Small <tylersmall.user@gmail.com>
2018-07-10 13:40:05 -0500

    Add player object w/ annotations

---
+++ APA-API/APA-API.Core/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APA_API.Core
+{
+    public class Player
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        public int SkillLevel { get; set; }
+
+        [Timestamp]
+        public byte[] Version { get; set; }
+    }
+}
