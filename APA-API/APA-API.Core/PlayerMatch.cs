8e0af0ad4f58bf99f350b7ea5f8ef141e20acfb0
Tyler Small <tylersmall.user@gmail.com>
2018-07-10 14:15:59 -0500

    Add foreign key constraints

--- APA-API/APA-API.Core/PlayerMatch.cs
+++ APA-API/APA-API.Core/PlayerMatch.cs
@@ -6,10 +6,14 @@ namespace APA_API.Core
     public class PlayerMatch
     {
         public int ID { get; set; }
+        [Required]
+        public int TeamMatchId { get; set; }
         public TeamMatch TeamMatch { get; set; }
         [Required]
+        public int PlayerAId { get; set; }
         public Player PlayerA { get; set; }
         [Required]
+        public int PlayerBId { get; set; }
         public Player PlayerB { get; set; }
         [Required]
         public int PlayerAMustWin { get; set; }
--- APA-API/APA-API.Core/TeamMatch.cs
+++ APA-API/APA-API.Core/TeamMatch.cs
@@ -8,10 +8,13 @@ namespace APA_API.Core
         public int ID { get; set; }
         [MaxLength(50)]
         public string Location { get; set; }
+        public int HostTeamId { get; set; }
         public Team HostTeam { get; set; }
         [Required]
+        public int TeamAId { get; set; }
         public Team TeamA { get; set; }
         [Required]
+        public int TeamBId { get; set; }
         public Team TeamB { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
